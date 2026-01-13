ff97922adab4fe58cb3cf191b2aa35ee66a31f29
Salehahmedeus <ahmedfox99999@gmail.com>
2026-01-13 18:44:02 +0200

    feat: add dependencies and api endpoints for content management

--- lib/core/constants/api_endpoints.dart
+++ lib/core/constants/api_endpoints.dart
@@ -9,4 +9,6 @@ class ApiEndpoints {
   static const String verifyOtp = '/otp/verify';
   static const String logout = '/logout';
   static const String profile = '/me';
+  static const String materials = '/materials';
+  static const String uploadMaterial = '/materials/upload';
 }
